6eba0d803bb0934179339d8799fb8084d22eaeb0
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-07-30 07:27:40 -0500

    feat: updates player script and scene for attackstate

--- player/player.gd
+++ player/player.gd
@@ -15,6 +15,9 @@ func _physics_process(_delta: float) -> void:
 		if input_vector != Vector2.ZERO:
 			var direction_vector = Vector2(input_vector.x, -input_vector.y)
 			update_blend_positions(direction_vector)
+			
+		if Input.is_action_just_pressed("attack"):
+			playback.travel("AttackState")
 		
 		velocity = input_vector * SPEED
 		move_and_slide()
@@ -25,3 +28,4 @@ func _physics_process(_delta: float) -> void:
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position", direction_vector)
